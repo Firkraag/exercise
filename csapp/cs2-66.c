4d3c48bfce6e08038d4f63117c64c1088c22451e
Firkraag <cntqrxj@gmail.com>
2014-03-28 01:16:47 +0800

    midnight

--- csapp/cs2-66.c
+++ csapp/cs2-66.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 
-int leftmost_one(unsigned x) {
+//Generate mask indicating leftmost 1 in x. Assume w=32. For example 0xFF00 -> 0x8000, and 0x6600 --> 0x4000. If x = 0, then return 0.
+int rightmost_one(unsigned x) {
+//	int y = -((int) x);
+
+//	printf("0x%x, 0x%x, 0x%x\n", y, x,  y & x);
+	return -x & x;
+}
+
+
+main() {
+	printf("0x%x, 0x%x, 0x%x\n", rightmost_one(0xfffffff0), rightmost_one(0x6600), rightmost_one(0x8000));
+}
 	
