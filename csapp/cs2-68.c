5298d4250231a03f74db88382ff88afab38e363d
Firkraag <cntqrxj@gmail.com>
2014-03-29 00:07:10 +0800

    cs2-69.c

--- csapp/cs2-68.c
+++ csapp/cs2-68.c
@@ -2,10 +2,11 @@
 /* Clear all but least significant n bits of x 
 Examples: x = 0x78ABCDEF, n = 8 --> 0xEF, n = 16 --> 0xCDEF
 Assume 1 <= n <= w */
+
 int lower_bits(int x, int n) {
 	int mask = (1 << n) - 1;
 	return mask & x;
 }
-main() {
-	printf("%x\n", lower_bits(0x12345678, 8));
-}
+//main() {
+	//printf("0x%x\n", lower_bits(0x12345678, 10));
+//}
---
+++ csapp/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+main() {
+	printf("0x%x\n", rotate_right(0x12345678, 32));
+}
+
