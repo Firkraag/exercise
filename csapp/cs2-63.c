486a08445e3673ec37ef74c6464b3aac64defd42
Firkraag <cntqrxj@gmail.com>
2014-03-29 15:16:19 +0800

    cs2-63.c

--- csapp/cs2-63.c
+++ csapp/cs2-63.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-// judge the kth bit of x, if it is 1, return 1, otherwise return 0
+//Judge the kth bit of x, if it is 1, return 1, otherwise return 0
+//int bit(int x, int k) {
+	//return 0x1 & (x >> k - 1);
+//	
+//} 
+//Another version
 int bit(int x, int k) {
-	return 0x1 & (x >> k - 1);
-	
-} 
+	return !!((0x1 << k - 1) & x);
+}
 
 //Function sra performs an arithmetic right shift using a logical right shift given by value xsrl
 int sra(int x, int k) {
