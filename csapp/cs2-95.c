841c9034c34201c476249c2c7ec55e5daab30cd5
Firkraag <cntqrxj@gmail.com>
2014-04-16 23:21:11 +0800

    love

---
+++ csapp/cs2-95.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <limits.h>
+
+typedef unsigned float_bits;
+
+/* Determine the position of leftmost one */	
+int leftmost_one_pos(unsigned x) {
+	int high = 31;
+	int low = 0;
+	int middle;
+	int result;
+
+	while (low <= high) {
+		middle = (low + high) / 2;
+		result = x >> middle;
+		if (result == 0x1)
+			return middle + 1;
+		else if (result > 0x1)
+			low = middle + 1;
+		else
+			high = middle - 1;
+	}
+}
+
+/* Compute (float) i */
+float_bits float_i2f(int i) {
+	//unsigned sign = f >> 31;
+	//unsigned exp = f >> 23 & 0xff;
+	//unsigned frac = f & 0x7fffff;
+	unsigned sign;
+	unsigned exp;
+	unsigned frac;
+	unsigned round_two_bits;
+	int pos;
+	
+	if (i < 0) {
+		sign = 1;
+		i = -i;
+	}
+	else 
+		sign = 0;
+	pos = leftmost_one_pos(i);
+	
+	if (pos <= 24 && pos > 0) {
+		frac = (i << (24 - pos)) & 0x7fffff; 
+		exp = pos + 126;
+	}
+	else if (pos > 24 && pos < 32) {
+		frac = (i >> (pos - 24)) & 0x7fffff;
+		round_two_bits = (i >> (pos - 25)) & 0x3;
+		if (round_two_bits == 0x3)
+			frac = frac + 1;
+		exp = pos + 126;
+	}
+	else if (pos == 32) 
+		return 0xcf000000;
+	else 
+		return 0x0;
+	return (sign << 31) | (exp << 23) | frac;
+}
+
+int main() {
+	int i;
+	
+	printf("0x%x\n", float_i2f(0x01000003));
+}
