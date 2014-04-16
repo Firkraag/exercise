cc26ca9f2168f4e285ef75de66e99f70f6788f5b
Firkraag <cntqrxj@gmail.com>
2014-04-16 21:01:51 +0800

    love

--- csapp/cs2-66.c
+++ csapp/cs2-66.c
@@ -3,6 +3,9 @@
 int rightmost_one(unsigned x) {
 	return -x & x;
 }
+int rightmost_zero(unsigned x) {
+	return rightmost_one(~x);
+}
 
 //Generate mask indicating leftmost 1 in x. Assume w=32. For example 0xFF00 -> 0x8000, and 0x6600 --> 0x4000. If x = 0, then return 0.
 int leftmost_one(unsigned x) {
@@ -19,12 +22,15 @@ int leftmost_one(unsigned x) {
 	x -= (x >> 1);
 	return x;
 }
+int leftmost_zero(unsigned x) {
+	return leftmost_one(~x);
+}	
 	
-//main() {
+main() {
 	//printf("0x%x, 0x%x, 0x%x\n", rightmost_one(0xfffffff0), rightmost_one(0x6600), rightmost_one(0x8000));
 	//printf("0x%x\n", 1 << 17);
 	//printf("0x%x\n", 0xff99 ^ 0xff96);
 	//printf("0x%x\n", (0x1 << 24) - 1);
 	//printf("0x%x\n", leftmost_one(0x03000000));
-//}
-	
+	printf("0x%x, 0x%x\n", leftmost_zero(0xffffff00), rightmost_zero(0xff));
+}
