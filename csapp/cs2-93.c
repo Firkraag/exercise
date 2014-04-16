a45035e9f17c95c83d3ab2af64e4dc8263b0ecf5
Firkraag <cntqrxj@gmail.com>
2014-04-16 08:14:20 +0800

    love

---
+++ csapp/cs2-93.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef unsigned float_bits;
+
+/* Compute 2*f. If f is NaN, then return f. */
+float_bits float_twice(float_bits f) {
+	unsigned sign = f >> 31;
+	unsigned exp = (f >> 23) & 0xff;
+	unsigned frac = f & 0x7FFFFF;
+
+	if (exp == 0xff && frac != 0)
+		return f;
+	else {
+		sign = sign ^ 1;
+		return (sign << 31) | (exp << 23) | frac;
+	}
+}
+
+main() {
+	printf("0x%x\n", float_negate(0xff800001));
+}
