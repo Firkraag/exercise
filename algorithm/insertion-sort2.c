35a8096b3e2997dbd7e73f780fd3e87ab4432f75
Firkraag <cntqrxj@gmail.com>
2014-04-28 12:02:01 +0800

    lost and calm

--- algorithm/insertion-sort2.c
+++ algorithm/insertion-sort2.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+//Sort into nonincreasing order
 void insertionsort(int s[], int length) {
 	int j, i;
 	int key;
+
 	for (j = 1; j < length; j++) {
 		key = s[j];
 		for (i = j - 1; i >= 0 && s[i] < key; i--)
