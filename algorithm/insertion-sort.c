4018b9858bd9f90b611579737c21c5aa55d3dd65
Firkraag <cntqrxj@gmail.com>
2014-05-17 22:52:47 +0800

    chaos

--- algorithm/insertion-sort.c
+++ algorithm/insertion-sort.c
@@ -19,8 +19,8 @@ void insertionsort(int s[], int length) {
 		for (i = j - 1; i >= 0 && s[i] > key; i--)
 			s[i+1] = s[i];
 		s[i+1] = key;
-		for (i = 0; i < length; i++)
-			printf("%d ", s[i]);
-		printf("\n");
+//		for (i = 0; i < length; i++)
+//			printf("%d ", s[i]);
+//		printf("\n");
 	}
 }
