313bb197782da6464bda8d4548fb9817afece61b
Firkraag <cntqrxj@gmail.com>
2015-09-08 11:41:02 +0800

    cut rod

--- algorithm/bottom_up_cut_rod.py
+++
@@ -1,8 +0,0 @@
-def bottom_up_cut_rod(p, n):
-	r = [0] * (n + 1)
-	for j in range(1, n + 1):
-		q = float("-Inf")
-		for i in range(1, j + 1):
-			q = max(q, p[i - 1] + r[j - i])
-		r[j] = q
-	return r[n]	
