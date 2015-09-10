c2eadd8afd26aa3cffe50cf3b42c1eee758b27e8
Firkraag <cntqrxj@gmail.com>
2015-09-11 01:11:19 +0800

    add priority queue and huffman code

--- algorithm/heap.py
+++ algorithm/heap.py
@@ -1,31 +1,62 @@
-class heap(list):
+class max_heap(object):
 	def __init__(self, data):
-		self.__heap = data
-		self.__length = len(data)
-		self.__heap_size = self.__length
+		self.heap = data
+		self.length = len(data)
+		self.heap_size = self.length
+		self.build_max_heap()
 	def left(self, i):
 		return 2 * i + 1
 	def right(self, i):
 		return 2 * i + 2
+	def parent(self, i):
+		return (i - 1) / 2
 	def max_heapify(self, i):
 		l = self.left(i)
 		r = self.right(i)
-		if (l <= (self.__heap_size - 1)) and (self.__heap[l] > self.__heap[i]):
+		if (l <= (self.heap_size - 1)) and (self.heap[l] > self.heap[i]):
 			largest = l
 		else:
 			largest = i
-		if (r <= (self.__heap_size - 1)) and (self.__heap[r] > self.__heap[largest]):
+		if (r <= (self.heap_size - 1)) and (self.heap[r] > self.heap[largest]):
 			largest = r
 		if 	largest != i:
-			self.__heap[i],self.__heap[largest] = self.__heap[largest],self.__heap[i]
+			self.heap[i],self.heap[largest] = self.heap[largest],self.heap[i]
 			self.max_heapify(largest)
 	def build_max_heap(self):
-		self.__heap_size = self.__length
-		for i in range(self.__length / 2 - 1, -1, -1):
+		self.heap_size = self.length
+		for i in range(self.length / 2 - 1, -1, -1):
 			self.max_heapify(i)
 	def heapsort(self):
 		self.build_max_heap()
-		for i in range(self.__length - 1, 0, -1):
-			self.__heap[0],self.__heap[i] = self.__heap[i],self.__heap[0]
-			self.__heap_size = self.__heap_size - 1
+		for i in range(self.length - 1, 0, -1):
+			self.heap[0],self.heap[i] = self.heap[i],self.heap[0]
+			self.heap_size = self.heap_size - 1
 			self.max_heapify(0)
+class min_heap(object):
+	def __init__(self, data):
+		self.heap = data
+		self.length = len(data)
+		self.heap_size = self.length
+		self.build_min_heap()
+	def left(self, i):
+		return 2 * i + 1
+	def right(self, i):
+		return 2 * i + 2
+	def parent(self, i):
+		return (i - 1) / 2
+	def min_heapify(self, i):
+		l = self.left(i)
+		r = self.right(i)
+		if (l <= (self.heap_size - 1)) and (self.heap[l] < self.heap[i]):
+			smallest = l
+		else:
+			smallest = i
+		if (r <= (self.heap_size - 1)) and (self.heap[r] < self.heap[smallest]):
+			smallest = r
+		if 	smallest != i:
+			self.heap[i],self.heap[smallest] = self.heap[smallest],self.heap[i]
+			self.min_heapify(smallest)
+	def build_min_heap(self):
+		self.heap_size = self.length
+		for i in range(self.length / 2 - 1, -1, -1):
+			self.min_heapify(i)
--- algorithm/heap_test.py
+++ algorithm/heap_test.py
@@ -1,5 +1,5 @@
 import unittest
-from heap import heap
+from heap import max_heap, min_heap
 
 class TestHeap(unittest.TestCase):
 #	def test_init(self):
@@ -8,17 +8,22 @@ class TestHeap(unittest.TestCase):
 #		self.assertEquals(a.__length, 10)
 #		self.assertEquals(a.__heap_size, 10)
 	def test_max_heapify(self):
-		pirnt 'ABC'
 		a = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
-		heap(a).max_heapify(1)
+		max_heap(a).max_heapify(1)
 		self.assertEquals(a, [16, 14, 10, 8, 7, 9, 3, 2, 4, 1])
 	def test_build_max_heap(self):
 		a = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
-		heap(a).build_max_heap()
+		max_heap(a).build_max_heap()
 		self.assertEquals(a, [16, 14, 10, 8, 7, 9, 3, 2, 4, 1])
 	def test_heapsort(self):
-		a = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
-		heap(a).heapsort()
-		print a
+		a = [4, 1, 3, 3, 16, 9, 10, 14, 8, 7]
+		max_heap(a).heapsort()
 		self.assertEquals(a, [1, 3, 3, 4, 7, 8, 9, 10, 14, 16])
-		
+	def test_min_heapify(self):
+		a = [1, 10, 3, 2, 7, 8, 9, 4, 14, 16]
+		min_heap(a).min_heapify(1)
+		self.assertEquals(a, [1, 2, 3, 4, 7, 8, 9, 10, 14, 16])
+	def test_build_min_heap(self):
+		a = [1, 10, 3, 2, 7, 8, 9, 4, 14, 16]
+		min_heap(a).build_min_heap()
+		self.assertEquals(a, [1, 2, 3, 4, 7, 8, 9, 10, 14, 16])
