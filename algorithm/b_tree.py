399111f4906ae7edb78f86145d709487278aed4b
Firkraag <cntqrxj@gmail.com>
2015-09-14 14:48:52 +0800

    fix b_tree bug

--- algorithm/b_tree.py
+++ algorithm/b_tree.py
@@ -67,6 +67,93 @@ class b_tree_node(object):
 				self.c[i - 1].print_child_first()
 		for i in range(1, self.n + 1):
 			print self.key[i - 1],
+	def delete(self, tree, k):
+		t = self.t
+		i = 1
+		while i <= self.n and k > self.key[i - 1]:
+			i = i + 1
+		if i <= self.n and k == self.key[i - 1]:
+			if self.leaf:
+				for j in range(i, self.n):
+					self.key[j - 1] = self.key[j]
+				self.n = self.n - 1
+			else:
+				y = self.c[i - 1]
+				z = self.c[i]
+				if y.n >= t:
+					p = y
+					while not p.leaf:
+						p = p.c[p.n]
+					key = p.key[p.n - 1]
+					y.delete(tree, key)
+					self.key[i - 1] = key
+				elif z.n >= t:
+					s = z
+					while not s.leaf:
+						s = s.c[0]
+					key = s.key[0]
+					z.delete(tree, key)
+					self.key[i - 1] = key
+				else:
+					self.merge(tree, i)
+					y.delete(tree, k)
+		elif self.leaf:
+			return None
+		elif self.c[i - 1].n <= t - 1:
+			if i <= self.n and self.c[i].n >= t:
+				a = self.c[i - 1]
+				b = self.c[i]
+				a.key[a.n] = self.key[i - 1]
+				self.key[i - 1] = b.key[0]
+				for j in range(2, b.n + 1):
+					b.key[j - 2] = b.key[j - 1]
+				a.c[a.n + 1] = b.c[0]
+				for j in range(1, b.n + 1):
+					b.c[j - 1] = b.c[j]
+				a.n = a.n + 1
+				b.n = b.n - 1
+				a.delete(tree, k)
+			elif i == self.n + 1 and self.c[i - 2].n >= t:
+				b = self.c[i - 2]
+				a = self.c[i - 1]
+				for j in range(1, a.n + 1):
+					a.key[j] = a.key[j - 1]
+				a.key[0] = self.key[i - 2]
+				self.key[i - 2] = b.key[b.n - 1]
+				for j in range(1, a.n + 2):
+					a.c[j] = a.c[j - 1]
+				a.c[0] = b.c[b.n]
+				b.n = b.n - 1
+				a.n = a.n + 1
+				a.delete(tree, k)
+			elif i <= self.n:
+				self.merge(tree, i)
+				self.c[i - 1].delete(tree, k)
+			else:
+				self.merge(tree, i - 1)
+				self.c[i - 2].delete(tree, k)
+		else:
+			self.c[i - 1].delete(tree, k)		
+	def merge(self, tree, i):
+		y = self.c[i - 1]
+		z = self.c[i]
+		t = y.t
+		yn = y.n
+		zn = z.n
+		y.key[t - 1] = self.key[i - 1]
+		for j in range(1, zn + 1):
+			y.key[j + yn] = z.key[j - 1]
+		if not y.leaf:
+			for j in range(1, zn + 2):
+				y.c[j + yn] = z.c[j - 1]
+		y.n = yn + zn + 1
+		for j in range(i, self.n):
+			self.key[j - 1] = self.key[j]
+		for j in range(i + 1, self.n + 1):
+			self.c[j - 1] = self.c[j]
+		self.n = self.n - 1
+		if tree.root == self and self.n == 0:
+			tree.root = y
 class b_tree(object):
 	def __init__(self, t):
 		self.t = t
